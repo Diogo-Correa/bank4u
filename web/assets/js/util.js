aec448a4b57f8ded5581f3d4364e60dc29f6e4b7
Diogo Corrêa <diogocorrea@id.uff.br>
2021-11-17 18:50:43 -0300

    first commit

---
+++ web/assets/js/util.js
@@ -0,0 +1,80 @@
+$( document ).ready(function() {
+
+    // Definições de Máscaras
+    $(".mascara-data").mask("99/99/9999");
+    $(".mascara-cpf").mask("999.999.999-99");
+    $(".mascara-cnpj").mask("99.999.999/9999-99");
+    $(".mascara-telefone").mask("(99)9999-9999?9");
+    $(".mascara-cep").mask("99.999-999");
+
+    $('.mascara-telefone').focusout(function(){
+     let phone, element;
+     element = $(this);
+     element.unmask();
+     phone = element.val().replace(/\D/g, '');
+     if(phone.length > 10) {
+         element.mask("(99)99999-999?9");
+     } else {
+         element.mask("(99)9999-9999?9");
+     }
+ });
+});
+
+function ValidaCPF(cpfEnviado) {
+  Object.defineProperty(this, 'cpfLimpo', {
+    enumerable: true,
+    get: function() {
+      return cpfEnviado.replace(/\D+/g, '');
+    }
+  });
+}
+
+ValidaCPF.prototype.valida = function() {
+  if(this.cpfLimpo.length !== 11 || this.isSequencia() || typeof this.cpfLimpo === 'undefined') return false;
+
+  const cpfParcial = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
+  const digito2 = this.criaDigito(cpfParcial + digito1);
+  
+  const novoCpf = cpfParcial + digito1 + digito2;
+  
+  return novoCpf === this.cpfLimpo;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+  const cpfArray = Array.from(cpfParcial);
+
+  let regressivo = cpfArray.length + 1;
+  const total = cpfArray.reduce((ac, val) => {
+    ac += (regressivo * Number(val));
+    regressivo--;
+    return ac;
+  }, 0);
+
+  const digito = 11 - (total % 11);
+  return digito > 9 ? '0' : String(digito);
+};
+
+ValidaCPF.prototype.isSequencia = function() {
+  const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+  return sequencia === this.cpfLimpo;
+};
+
+function checkCPF(value) {
+    const cpfInput = document.getElementById('cpf');
+    const passInput = document.getElementById('senha');
+    const loginBtn = document.getElementById('loginBtn');
+    const isValid = new ValidaCPF(value).valida();
+    
+    if(isValid) {
+      cpfInput.classList.remove('is-invalid');
+      cpfInput.classList.add('is-valid');
+      passInput.classList.remove('d-none');
+      loginBtn.disabled = false;
+    } else {
+      cpfInput.classList.add('is-invalid'); 
+      passInput.classList.add('d-none');
+      loginBtn.disabled = true;
+    }
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : index
+    Created on : 15/11/2021, 20:54:17
+    Author     : Diogo
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <jsp:include page="./assets/includes/head.html" />
+</head>
+<body>
+
+    <jsp:include page="./assets/includes/content.html" />
+
+    <jsp:include page="./assets/includes/scripts.html" />
+</body>
+</html>
